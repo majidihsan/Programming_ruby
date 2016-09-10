267d5be0e784b43c2f396634b5d38ac75da3cfdc
majid ihsan <majidihsan@gmail.com>
2016-09-09 22:55:05 -0400

    3rd commit

--- programming_ruby.rb
+++ programming_ruby.rb
@@ -62,3 +62,42 @@ puts "Price = #{book.price}"
 #Produces
 #ISBN = isbn1
 #Price = 3.14 (because we have defined two acessor methods to get the value of two instance varirables)
+
+class BookInStock
+  attr_reader :isbn, :price
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+end
+
+book = BookInStock.new("isbn1", 3.14)
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
+
+#produces the same result as ruby has provided a convenient method called attr_reader.
+
+#WRITABLE ATTRIBUTES
+
+class BookInStock
+  attr_reader :isbn, :price
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+  def price=(new_price)
+    @price = new_price
+  end
+end
+
+book = BookInStock.new("isbn1", 3.14)
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
+book.price = book.price * 0.75
+puts "New Price = #{book.price}"
+
+#produces
+#ISBN = isbn1
+#Price = 3.14
+#New Price = 2.355
+#Note you can get the same result by making the price as atter_accessor.
